01aec2b0a942e13fea56d900705dde48f7085970
Andrew Crites <acrites@mobiquityinc.com>
2016-02-11 13:15:07 -0500

    Storing created images on Cloudinary

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const connect = require("connect");
 const app = connect();
 const himawari = require("himawari");
 const moment = require("moment");
+const cloudinary = require("cloudinary");
+const axios = require("axios");
 
 app.use(require("body-parser").urlencoded({extended: true}));
 app.use((req, res) => {
@@ -13,10 +15,9 @@ app.use((req, res) => {
     const hostname = `${proto}://${host}`;
 
     if ("/" === req.url) {
-        res.setHeader("Content-Type", "application/json");
         if (req.body.token !== process.env.SLACK_TOKEN) {
             res.writeHead(401);
-            return res.end('{"error":"invalid token"}');
+            return res.end("invalid token");
         }
 
         let date = moment();
@@ -25,24 +26,31 @@ app.use((req, res) => {
         }
         if (!date.isValid()) {
             res.writeHead(401);
-            return res.end('{"error":"invalid date, dingus"}');
+            return res.end("invalid date, dingus");
         }
 
         const timestamp = +date;
         const filename = `himawari-${timestamp}.jpg`;
         const outfile = `/tmp/${filename}`;
 
+        res.end();
         himawari({
             date: date.toDate(),
             outfile,
             success() {
-                res.end(JSON.stringify({
-                    text: "",
-                    attachments: [{
-                        fallback: "Couldn't show the earth :(",
-                        image_url: `${hostname}/${filename}`,
-                    }],
-                }));
+                let stream = cloudinary.uploader.upload_stream(
+                    () => {},
+                    {public_id: filename, format: 'jpg'}
+                );
+                fs.createReadStream(outfile).pipe(stream).on('end', () => {
+                    axios.post(req.body.response_url, {
+                        text: "",
+                        attachments: [{
+                            fallback: "Couldn't show the earth :(",
+                            image_url: `${hostname}/${filename}`,
+                        }],
+                    });
+                });
             },
             error(err) {
                 console.error(err.stack);
@@ -52,13 +60,15 @@ app.use((req, res) => {
         });
     }
     else {
-        const stream = fs.createReadStream(`/tmp${req.url}`);
-        stream.on("open", () => stream.pipe(res));
-        stream.on("error", () => {
+        const url = cloudinary.url(req.url.slice(1));
+        if (url) {
+            http.get(`${url}.jpg`, (img) => img.pipe(res));
+        }
+        else {
             res.setHeader("Content-Type", "application/json");
             res.writeHead(404);
             res.end('{"error":"not found"}');
-        });
+        }
     }
 });
 
