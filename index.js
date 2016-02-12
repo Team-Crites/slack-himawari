00cc2b5fa5c0b94d75921e35131a82441d13b008
Andrew Crites <acrites@mobiquityinc.com>
2016-02-12 15:51:07 -0500

    Promisifying server + comments

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 'use strict';
-const fs = require("fs");
+const fs = require("mz/fs");
 const http = require("http");
+const co = require("co");
 const connect = require("connect");
 const app = connect();
 const himawari = require("himawari");
 const moment = require("moment");
-const cloudinary = require("cloudinary");
 const axios = require("axios");
 
 app.use(require("body-parser").urlencoded({extended: true}));
-app.use((req, res) => {
+app.use((req, res) => co(function* () {
+    // Determine host that Slack can access
     const proto = req.headers["x-forwarded-proto"] || "http";
     const host = req.headers["host"] || "localhost";
     const hostname = `${proto}://${host}`;
 
+    // Root command is the himawari command -- generate the image
     if ("/" === req.url) {
         if (req.body.token !== process.env.SLACK_TOKEN) {
-            res.writeHead(401);
-            return res.end("invalid token");
+            let error = new Error("invalid token");
+            error.status = 401;
+            throw error;
         }
 
         let date = moment();
@@ -25,52 +28,60 @@ app.use((req, res) => {
              date = moment(req.body.text);
         }
         if (!date.isValid()) {
-            res.writeHead(401);
-            return res.end("invalid date, dingus");
+            let error = new Error("invalid date, dingus");
+            error.status = 400;
+            throw error;
         }
 
         const timestamp = +date;
         const filename = `himawari-${timestamp}.jpg`;
         const outfile = `/tmp/${filename}`;
 
+        // Image generation takes some time; we'll use the Slack callback URL
         res.end();
+
+        const dfd = Promise.defer();
         himawari({
             date: date.toDate(),
             outfile,
+            // TODO https://github.com/jakiestfu/himawari.js/pull/7
             success() {
-                let stream = cloudinary.uploader.upload_stream(
-                    () => {},
-                    {public_id: filename, format: 'jpg'}
-                );
-                fs.createReadStream(outfile).pipe(stream).on('end', () => {
-                    axios.post(req.body.response_url, {
-                        text: "",
-                        attachments: [{
-                            fallback: "Couldn't show the earth :(",
-                            image_url: `${hostname}/${filename}`,
-                        }],
-                    });
-                });
+                dfd.resolve();
             },
-            error(err) {
-                console.error(err.stack);
-                res.writeHead(500);
-                res.end('{"error":"server error"}');
+            error() {
+                dfd.reject();
             },
+        });
+        yield dfd.promise;
+        // Respond with a path to the image
+        yield axios.post(req.body.response_url, {
+            text: "",
+            attachments: [{
+                fallback: "Couldn't show the earth :(",
+                image_url: `${hostname}/${filename}`,
+            }],
         });
     }
     else {
-        const url = cloudinary.url(req.url.slice(1));
-        if (url) {
-            http.get(`${url}.jpg`, (img) => img.pipe(res));
+        // An image has been requested. Emit it and then delete it.
+        const file = `/tmp${req.url}`;
+        if (yield fs.exists(file)) {
+            const stream = fs.createReadStream(file);
+            stream.pipe(res);
+            res.on("end", () => fs.unlink(file));
         }
         else {
-            res.setHeader("Content-Type", "application/json");
-            res.writeHead(404);
-            res.end('{"error":"not found"}');
+            let error = new Error("not found");
+            error.status = 404;
+            throw error;
         }
     }
-});
+}).catch(err => {
+    // Handle all errors thrown
+    console.error(err.stack);
+    res.writeHead(err.status || 500);
+    res.end(err.message);
+}));
 
 const server = http.createServer(app);
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () => console.log("Why was I programmed to feel pain??"));
